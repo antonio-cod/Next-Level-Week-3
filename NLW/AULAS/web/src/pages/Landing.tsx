5cc1c864a8a7d155448ce9e2cbff88283efe1b82
antonio-cod <67842667+antonio-cod@users.noreply.github.com>
2020-10-12 22:59:32 -0400

    NLW.EP-1.V2

--- NLW/AULAS/web/src/pages/Landing.tsx
+++ NLW/AULAS/web/src/pages/Landing.tsx
@@ -22,7 +22,7 @@ function Landing() {
       <span>Tangará da Serra.</span>
     </div>
 
-    <Link to="" className="enter-app">
+    <Link to="/app" className="enter-app">
       <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6 )" />
     </Link>
     </div>
